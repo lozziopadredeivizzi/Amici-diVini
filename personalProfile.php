f0cb0e53d4f4fa4cba8ace3f16a4c26f792b7ffe
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-03-20 15:45:12 +0100

    added exit option

--- personalProfile.php
+++ personalProfile.php
@@ -2,7 +2,7 @@
 <html lang="it">
 
 <head>
-    <meta charset="UTF-8"/>
+    <meta charset="UTF-8" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/profilo.css" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -31,13 +31,22 @@
             <div>
                 <button name="inseriscivino" id="newpost">Inserisci vino</button>
                 <button name="modificaprofilo" id="editprofile">Modifica Profilo</button>
+                <button name="esci" id="exit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                        fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
+                        <path fill-rule="evenodd"
+                            d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+                    </svg> Esci </button>
             </div>
             <ul>
                 <li>
-                    Breve descrizione:  <?php echo ($descrizione); ?>
+                    Breve descrizione:
+                    <?php echo ($descrizione); ?>
                 </li>
                 <li>
-                    Informazioni persoanli: <?php echo ($infoPersonali); ?>
+                    Informazioni persoanli:
+                    <?php echo ($infoPersonali); ?>
                 </li>
             </ul>
         </section>
@@ -174,4 +183,5 @@
     </nav>
     <script src="./javascript/goTo.js" type="text/javascript"></script>
 </body>
+
 </html>
